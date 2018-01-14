2e99d836c042c2bfd2ab77fcb8b9438aa5b7e9ef
aYangLi <18335909073@163.com>
2018-01-14 12:27:35 +0800

    发表评论完成

--- src/app/product-dtail/product-dtail.component.ts
+++ src/app/product-dtail/product-dtail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { Product, ProductService, Comment } from '../shared/product.service';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-product-dtail',
@@ -9,8 +11,15 @@ import { Product, ProductService, Comment } from '../shared/product.service';
 })
 export class ProductDtailComponent implements OnInit {
 
+  private titleFilter: FormControl = new FormControl();
+
   product: Product;
   comments: Comment[];
+
+  newRating: number = 5;
+  newComment: string = '';
+
+  isCommentHidden = true;
 
   constructor(private routeInfo: ActivatedRoute,
               private productService: ProductService,
@@ -21,5 +30,15 @@ export class ProductDtailComponent implements OnInit {
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
   }
+  addComment() {
+    let comment = new Comment(0, this.product.id, new Date().toISOString(), 'someone', this.newRating, this.newComment);
+    this.comments.unshift(comment);
 
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
+
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
+  }
 }
